68bcda45e4b668ccca49acc2d42e8a76dd66cfdc
David MacKintosh <dgawmackintosh@gmail.com>
2017-02-27 20:53:43 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// Libs
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
+// CSS 
+import './css/style.css';
+
+// Components
+import App from './components/App';
+import Home from './components/Home';
+
+// Render
+render((
+	<Router history={hashHistory}>
+	<Route path="/" component={Home} />
+	</Router>
+), document.getElementById('root')
+);
