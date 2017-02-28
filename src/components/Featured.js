41443a42a3a170f7fbefbe6ec964076fc71c27f7
User <user@iMac-CodeClan201.local>
2017-02-28 10:43:48 +0000

    Adds programmatic routes

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Featured = () => {  
+// This is a stateless functional component.
+// When a component is defined using an arrow function, props can be accessed by
+// passing it an argument (called props by convention).
+const Featured = props => {  
+  let topic = props.params.topic;
+  let name = props.params.name;
+
+
   return (
     <div className="main-content">
-      <h2>Featured: </h2>
-      <p>Introducing <strong> </strong>, a teacher who loves teaching courses about <strong> </strong>!</p>
+      <h2>{name}</h2>
+      <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong>{topic}</strong>!</p>
     </div>
   );
 }
--- src/router.js
+++ src/router.js
@@ -8,24 +8,26 @@ import Home from './components/Home';
 import About from './components/About';
 import Courses from './components/Courses';
 import Teachers from './components/Teachers';
-
-import HTML from './components/courses/HTML';
-import CSS from './components/courses/CSS';
-import JavaScript from './components/courses/JavaScript';
+import NotFound from './components/NotFound';
+import CourseContainer from './components/courses/CourseContainer';
+import CourseList from './data/courses';
+import Featured from './components/Featured';
 
 const routes = (
 	<Router history={browserHistory}>
 		<Route component={App}>
 			<Route path="/" component={Home} />
-			<Route path="about" component={About} />
+			<Route path="about" component={About} title="About" />
 			<Route path="about" component={About} />
 			<Route path="courses" component={Courses}>
 				<IndexRedirect to="html" />
-				<Route path="html" component={HTML} />
-				<Route path="css" component={CSS} />
-				<Route path="javascript" component={JavaScript} />
+				<Route path="html" component={CourseContainer} data={CourseList.HTML} />
+				<Route path="css" component={CourseContainer} data={CourseList.CSS} />
+				<Route path="javascript" component={CourseContainer} data={CourseList.JS} />
 			</Route>
 			<Route path="teachers" component={Teachers} />
+			<Route path="featured/:topic/:name" component={Featured} />
+			<Route path="*" component={NotFound} />
 		</Route>
 	</Router>
 );
